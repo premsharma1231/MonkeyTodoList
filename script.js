d8dce32d44af6354954511082a334a7c203ee2cf
premsharma1231 <149376221+premsharma1231@users.noreply.github.com>
2024-09-15 18:35:41 +0530

    Updated code with new features/fixes

--- script.js
+++ script.js
@@ -54,68 +54,33 @@ function saveData(){
     localStorage.setItem("data", listContainer.innerHTML);
 };
 
-function showList() {
-    // Retrieve stored list from localStorage or set to empty if none
-    const savedData = localStorage.getItem("data");
-    
-    if (savedData) {
-        // Restore list items
-        listContainer.innerHTML = savedData;
-
-        // Query the list items to ensure they are restored properly
-        const listItems = listContainer.querySelectorAll("li");
-
-        // Loop through each item to make sure all elements (like spans) are in place
-        listItems.forEach((li) => {
-            // If 'li' doesn't already have the span (close button), we add it
-            if (!li.querySelector('span')) {
-                let span = document.createElement("span");
-                span.innerHTML = '\u00d7';  
-                li.appendChild(span);
-            }
-
-            // Reapply event listeners for toggling 'checked' state
-            li.addEventListener('click', function(e) {
-                if (e.target.tagName === 'LI') {
-                    e.target.classList.toggle("checked");
-                    saveData();
-                } else if (e.target.tagName === "SPAN") {
-                    e.target.parentElement.remove();
-                    saveData();
-                }
-            });
-        });
-
-        // Apply GSAP animations to restored list items to make them visible
-        gsap.from(listItems, { opacity: 0, x: -100, duration: 0.3, stagger: 0.1 });
-    }
+function showList(){
+    listContainer.innerHTML = localStorage.getItem("data");
 }
-
-
 showList();
 
 
 resetTasksButton.addEventListener('click', ()=>{
-    gsap.to("#listContainer li", { 
-        opacity: 0, 
-        y: -20, 
-        duration: 0.5, 
-        stagger: 0.1,
-        onComplete: () => {
+    // gsap.to("#listContainer li", { 
+    //     opacity: 0, 
+    //     y: -20, 
+    //     duration: 0.5, 
+    //     stagger: 0.1,
+    //     onComplete: () => {
             listContainer.innerHTML = "";
             localStorage.setItem("data", listContainer.innerHTML);
             typeTasks.style.borderColor = 'white';
-        }
-    });
+    //     }
+    // });
 });
 
-listContainer.addEventListener("mouseover", function (e) {
-    if (e.target.tagName === "LI") {
-      gsap.to(e.target, { scale: 1.05, marginRight: '15px', marginLeft: '15px',duration: 0.2 });
-    }
-  });
-  listContainer.addEventListener("mouseout", function (e) {
-    if (e.target.tagName === "LI") {
-      gsap.to(e.target, { scale: 1, marginRight: '0', marginLeft: '0',duration: 0.2 });
-    }
-  });
+// listContainer.addEventListener("mouseover", function (e) {
+//     if (e.target.tagName === "LI") {
+//       gsap.to(e.target, { scale: 1.05, marginRight: '15px', marginLeft: '15px',duration: 0.2 });
+//     }
+//   });
+//   listContainer.addEventListener("mouseout", function (e) {
+//     if (e.target.tagName === "LI") {
+//       gsap.to(e.target, { scale: 1, marginRight: '0', marginLeft: '0',duration: 0.2 });
+//     }
+//   });
